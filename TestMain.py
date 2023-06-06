6cdf9eded2d51f49a6c8fb685bf97a6b2385dfc4
hagarashi8 <Artem501137@gmail.com>
2023-06-06 15:26:19 +0600

    unit tests

--- TestMain.py
+++ TestMain.py
@@ -2,11 +2,11 @@ import unittest
 from main import try_creds
 class TestCredentialVerification(unittest.TestCase):
     def test_granting_valid(self):
-        self.assertEqual(try_creds({"admin": "admin"}, "admin", "admin"))
+        self.assertEqual(try_creds({"admin": "admin"}, "admin", "admin"), True)
     def test_not_granting_invalid_password(self):
-        self.assertEqual(try_creds({"admin": "admin"}, "admin", "admins"))
+        self.assertEqual(try_creds({"admin": "admin"}, "admin", "admins"), False)
     def test_not_granting_invalid_user(self):
-        self.assertEqual(try_creds({"admin": "admin"}, "sadmin", "admin"))
+        self.assertEqual(try_creds({"admin": "admin"}, "sadmin", "admin"), False)
 
 if __name__ == '__main__':
     unittest.main()
