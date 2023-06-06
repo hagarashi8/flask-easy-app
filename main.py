cbe027aeefa320cf119ac0785f6c3e714724f101
hagarashi8 <Artem501137@gmail.com>
2023-06-06 14:31:11 +0600

    Guess that's enough

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from json import load
+if __name__ == '__main__':
+    users = dict()
 
-app = Flask(__name__)
+    with open("users.json") as usersfile:
+        users = load(usersfile)
+    app = Flask(__name__)
+    print(users)
+
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        return render_template("index.html")
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+    @app.route('/', methods=['post'])
+    def form():
+        if request.method == 'POST':
+            username = request.form.get('username').strip()
+            password = request.form.get('password').strip()
+            print("Lol")
+            if username in users.keys():
+                print("Kek")
+                if users[username] == password:
+                    print("Cheburek")
+                    return render_template('index.html', ans="Access granted")
+        return render_template('index.html', ans="Access denied")
+
+
+    app.run()
+
